0b55044fb575d751e956d6c14410cd224af3ac04
Bashar <basharalhajhamad9595@gmail.com>
2025-02-26 23:37:01 +0300

    Update invoices_controller.rb

--- app/controllers/public/api/v1/invoices_controller.rb
+++ app/controllers/public/api/v1/invoices_controller.rb
@@ -87,7 +87,7 @@ module Public
             Messages::MessageBuilder.new(user, @conversation, {
               content: message_content,
               account_id: 1, # Static account ID
-              inbox_id: 11,  # Static inbox ID
+              inbox_id: 2,  # Static inbox ID
               conversation_id: conversation_id,
               message_type: :outgoing
             }).perform
