ab12667827e88057cbe313e8f83639a4eecda766
Bashar <basharalhajhamad9595@gmail.com>
2025-02-26 23:07:49 +0300

    Update invoices_controller.rb

--- app/controllers/public/api/v1/invoices_controller.rb
+++ app/controllers/public/api/v1/invoices_controller.rb
@@ -52,7 +52,7 @@ module Public
             unless contact
               inbox = Inbox.find(inbox_id) # Fetch the inbox object
               contact_inbox = ::ContactInboxWithContactBuilder.new(
-                source_id: mobile_number, # Unique identifier for the contact in this inbox
+                source_id: mobile_number.gsub(/\D/, ''), # Unique identifier for the contact in this inbox
                 inbox: inbox, # Pass the inbox object, not the ID
                 contact_attributes: {
                   name: "Customer #{mobile_number}", # Default name
