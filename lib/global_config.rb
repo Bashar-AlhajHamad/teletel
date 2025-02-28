8905eaa9f38774cc4cf6c12a84570c8ac5583456
Bashar <basharalhajhamad9595@gmail.com>
2025-02-28 07:20:23 +0300

    Update global_config.rb

--- lib/global_config.rb
+++ lib/global_config.rb
@@ -53,7 +53,7 @@ class GlobalConfig
     def db_fallback(config_key)
       case config_key
       when 'INSTALLATION_NAME'
-        '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: Teletel\n'
+        '"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: Teletel\n"'
       when 'BRAND_URL'
         '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: https://www.teletel.io\n'
       when 'WIDGET_BRAND_URL'
