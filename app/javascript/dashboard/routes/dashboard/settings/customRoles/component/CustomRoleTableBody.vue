a3420d66b61d80008c70a537dd820b9a8462ed1c
Bashar <basharalhajhamad9595@gmail.com>
2025-04-04 11:22:34 +0300

    Revert "add custom roles"
    This reverts commit a47e627459d2972f59ea1395e6d6dfd25802c6c5.

--- app/javascript/dashboard/routes/dashboard/settings/customRoles/component/CustomRoleTableBody.vue
+++ app/javascript/dashboard/routes/dashboard/settings/customRoles/component/CustomRoleTableBody.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { useI18n } from 'vue-i18n';
+import { getI18nKey } from 'dashboard/routes/dashboard/settings/helper/settingsHelper';
+
 defineProps({
   roles: {
     type: Array,
@@ -12,25 +15,11 @@ defineProps({
 
 const emit = defineEmits(['edit', 'delete']);
 
+const { t } = useI18n();
+
 const getFormattedPermissions = role => {
-  // Ensure role.permissions is a valid array
-  if (
-    !role.permissions ||
-    !Array.isArray(role.permissions) ||
-    role.permissions.length === 0
-  ) {
-    return 'No permissions assigned';
-  }
-
   return role.permissions
-    .map(event => {
-      if (!event || typeof event !== 'object') {
-        return 'Invalid permission';
-      }
-
-      // Ensure event.action exists
-      return event.action ? event.action : 'Unknown action';
-    })
+    .map(event => t(getI18nKey('CUSTOM_ROLE.PERMISSIONS', event)))
     .join(', ');
 };
 </script>
